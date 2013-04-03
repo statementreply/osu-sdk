7f44eadf8f8a92c4db167ced9720d9cd51012fb2
statementreply <statementreply@gmail.com>
2013-04-03 12:12:48 +0800

    initial statstars project setup

---
+++ statstars/StatStars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using osu.GameModes.Edit.AiMod;
+using osu.GameplayElements.HitObjects;
+
+namespace StatStars
+{
+    /// <summary>
+    /// statementreply's star difficulty algorithm
+    /// </summary>
+    public class StatStars : AiModRuleset
+    {
+        public override AiModType Type
+        {
+            get { return AiModType.Meta; }
+        }
+
+        protected override void RunAllRules(List<HitObjectBase> hitObjects)
+        {
+            //Do your processing in here
+            Reports.Add(new AiReport(Severity.Info, "statstars: zzz"));
+        }
+    }
+}
